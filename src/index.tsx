aca6fc740488c0d0f259d64962b4c21e04860fc1
Deny <denykpamuji@gmail.com>
2024-12-09 17:00:19 +0700

    feat: - add routing - add sidebar menu

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<h1>Welcome to the App</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+
+        {/* Dashboard Routes */}
+        <Route path="/dashboard" element={<App />}>
+          {/* <Route
+            path="invoice/documents"
+            element={<h1>Invoice Document Page</h1>}
+          /> */}
+          <Route path="invoice">
+            <Route path="documents" element={<h1>Invoice Document Page</h1>} />
+            <Route path="templates" element={<h1>Invoice Template Page</h1>} />
+          </Route>
+          <Route path="offer-letter">
+            <Route
+              path="documents"
+              element={<h1>Offer Letter Document Page</h1>}
+            />
+            <Route
+              path="templates"
+              element={<h1>Offer Letter Template Page</h1>}
+            />
+          </Route>
+          <Route path="setting" element={<h1>Setting Page</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
